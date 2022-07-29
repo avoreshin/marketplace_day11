0931a57e05fefdfa5578a2471d267aebec435d87
avoreshin <avoreshin@gmail.com>
2022-07-29 23:55:45 +0300

    feat : AppBar

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import styled from "styled-components/macro";
+import {Card} from "@mui/material";
+import logo from "../logo.svg";
 
 function ProductCard(props) {
-    const {photo, title, price} = props;
+    const {title, price} = props.data
 
+    console.log(props)
 
     return (
-        <div>Card</div>
+        <div css={` padding: 20px;
+          width: 300px;
+          background-color: #292D39;
+          border-radius: 3px;
+          border: 1px solid #F7F7F7;
+
+          :hover {
+            background-color: rgba(41, 45, 57, 0.52);
+            border: 1px solid rgba(231, 232, 245, 0.52);
+          }
+        `}>
+
+            <img src={logo} alt=""/>
+            <div css={`display: block`}>
+                <span css={`font-size: 36px;
+                  color: #39D88F`}>{title}</span>
+                <span css={`font-size: 36px;
+                  color: #F2F2F2;
+                  float: right`}>{price}</span>
+            </div>
+        </div>
     );
 }
 
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,18 +1,55 @@
 import React, {useState} from 'react';
 import ProductCard from "../components/ProductCard";
+import logo from "../logo.svg";
+import App from "../App";
+import {AppBar, Container, IconButton,  ListItem, Toolbar} from "@mui/material";
+import Bange from "@mui/material/Badge"
+import styled from "styled-components/macro";
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
 
 function MainPage(props) {
-    const [search, setSeatch] =useState('');
-    const [data, setData] = useState('')
-    const [basket, setBasket] =useState('')
+    const [search, setSeatch] = useState('');
+    const [data, setData] = useState([{price: "1000", title: "Milk"}]);
+    const [basket, setBasket] = useState('')
+    const [count, setCount] = useState(0)
     const [login, setLogin] = useState('false')
 
     return (
 
-        <div>
-            {!data && data.map((item, index) =>{
-                return <ProductCard key = {index} data = {item}/>
-            })}
+        <div css={`background-color: #282c34;
+          height: 100vh`}>
+
+            <AppBar
+                position="sticky" sx={{height: 100}}>
+                <Toolbar css={`background-color: #1E212A`}>
+                    <a href="/">
+                        <img src={logo} alt="logo"
+                             css={`height: 100px;
+                               animation: App-logo-spin infinite 20s linear`}/>
+                    </a>
+                    <div css={`float: right;
+                      margin: 0 100px 0 auto;
+                      color: #61dafb;`}>
+
+                        <IconButton onClick={()=>{return (setCount(count+1))}}>
+                            <Bange badgeContent={count} color={"error"}>
+                                <LocalGroceryStoreIcon fontSize={'large'} css={`color: #E26060;`}/>
+                            </Bange>
+                        </IconButton>
+
+                    </div>
+                </Toolbar>
+
+            </AppBar>
+            <Container>
+
+                {data && data.map((item, index) => {
+                    return (<ProductCard key={index} data={item}/>)
+                })}
+
+            </Container>
+
+
         </div>
     );
 }
