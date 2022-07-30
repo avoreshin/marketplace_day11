5c0541283df9dc7eded6629d8687eeab354e10bd
AngelinaCP <89658366303@mail.ru>
2022-07-30 11:29:13 +0500

    feat: endpoints:

--- server/app.js
+++ server/app.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const {sequelize} = require('./models')
 const {DataTypes} = require("sequelize");
+const Marketplace = require("./models/marketplace")
 
 const app = express();
+
+app.post('/cart', async(req, res) => {
+    const {title, description, photo, vendor_info, price} = req.body
+    try {
+        const marketplace = await Marketplace.create({title, description, photo, vendor_info, price})
+        return res.json(marketplace)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err);
+    }
+})
+
+app.get('/products ', async(req, res) => {
+    try {
+        const marketplace = await Marketplace().findAll({})
+        return res.json(marketplace)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err);
+    }
+})
+
+app.get('/products/:id', async(req, res) => {
+    const id = req.params.id
+    try {
+        const marketplace = await Marketplace().findOne({
+            where: {id}
+        })
+        return res.json(marketplace)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err);
+    }
+})
 
 app.use(express.json())
 
